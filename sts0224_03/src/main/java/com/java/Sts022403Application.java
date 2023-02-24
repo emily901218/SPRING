2b61d1a8bd2ec094e75f7e802fb5f90b9afc512f
KV021 <KV021@DESKTOP-M56O06D>
2023-02-24 18:35:27 +0900

    2023년 2월 24일 수업내용 summernote

---
+++ sts0224_03/src/main/java/com/java/Sts022403Application.java
@@ -0,0 +1,13 @@
+package com.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sts022403Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sts022403Application.class, args);
+	}
+
+}
